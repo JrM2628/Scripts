9d81b2ed406e891b7a66b587dcb17cfa50a7baf3
jrm7162@rit.edu <jrm7162@rit.edu>
2023-07-09 19:36:36 -0400

    2fa test

---
+++ 2fa.py
@@ -0,0 +1,19 @@
+import hmac, base64, struct, hashlib, time
+
+def get_hotp_token(secret, intervals_no):
+    key = base64.b32decode(secret, True)
+    msg = struct.pack(">Q", intervals_no)
+    h = hmac.new(key, msg, hashlib.sha1).digest()
+    o = h[19] & 15
+    h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
+    return h
+
+def get_totp_token(secret):
+    return get_hotp_token(secret, intervals_no=int(time.time())//30)
+
+def main():
+    secret = base64.b32encode("bruhmoment".encode()).decode()
+    print(get_totp_token(secret))
+
+if __name__ == "__main__":
+    main()
